42b43ca9a3c5ed4c7f4fa298ae437ec24b032df3
Andrea Fulgence <andreafulgence.ndreveloarisoa1@enetcom.u-sfax.tn>
2021-09-26 12:58:50 +0100

    first commit

---
+++ src/test/java/com/dksoft/prescriptionmanagement/PrescriptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dksoft.prescriptionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
